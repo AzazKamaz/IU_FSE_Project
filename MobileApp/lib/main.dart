8c1d50cfbbcbb851cc0fd8e85d1dea260ad64f73
Aleksandr Krotov <san.crot@yandex.ru>
2020-10-11 21:35:39 +0300

    Add BLE scanning and displaying

--- MobileApp/lib/main.dart
+++ MobileApp/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_ble_lib/flutter_ble_lib.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,39 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Automated Attendance BLE Test',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'BLE Peripheral list'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,67 +32,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  PermissionStatus locationPermissionStatus = PermissionStatus.unknown;
+  final BleManager bleManager = BleManager();
+  final Map<String, MapEntry<DateTime, ScanResult>> peripherals = {};
 
-  void _incrementCounter() {
+  @override
+  initState() {
+    super.initState();
+
+    bleManager.createClient().then((_) {
+      print("Client created");
+      clear();
+    });
+  }
+
+  Future<void> _checkPermissions() async {
+    if (Platform.isAndroid) {
+      while (locationPermissionStatus != PermissionStatus.granted) {
+        var permissionStatus = await PermissionHandler()
+            .requestPermissions([PermissionGroup.location]);
+
+        locationPermissionStatus = permissionStatus[PermissionGroup.location];
+      }
+
+      if (locationPermissionStatus != PermissionStatus.granted) {
+        return Future.error(Exception("Location permission not granted"));
+      }
+    }
+  }
+
+  Future<void> clear() async {
+    await _checkPermissions();
+
+    await bleManager.stopPeripheralScan();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      peripherals.clear();
+      print("Scanning...");
+      bleManager.startPeripheralScan().listen((peripheral) {
+        setState(() {
+          peripherals[peripheral.peripheral.identifier] = MapEntry(DateTime.now(), peripheral);
+        });
+      });
     });
   }
 
   @override
+  void dispose() {
+    bleManager.destroyClient();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: peripherals.values.where((p) =>
+          p.value.peripheral.name != null || p.value.advertisementData.serviceData != null || p.value.advertisementData.serviceUuids != null ||p.value.advertisementData.solicitedServiceUuids != null
+        ).map((p) => ListTile(
+          title: Text('[${p.value.peripheral.identifier}] ${p.value.peripheral.name ?? ''}'),
+          subtitle: Column(
+            children: [
+              Column(
+                children:
+                p.value.advertisementData.serviceData?.entries?.map(
+                        (service) => Text(service.value.length > 0
+                        ? '[${service.key}] - ${String.fromCharCodes(service.value)}'
+                        : '[${service.key}]'))?.toList() ?? [],
+              ),
+              Column(
+                children: p.value.advertisementData.serviceUuids?.map((service) => Text(service))?.toList() ?? [],
+              ),
+              Column(
+                children: p.value.advertisementData.solicitedServiceUuids?.map((service) => Text(service))?.toList() ?? [],
+              )
+            ],
+          ),
+        )).toList(),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: clear,
+        tooltip: 'Clear',
+        child: Icon(Icons.clear),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
