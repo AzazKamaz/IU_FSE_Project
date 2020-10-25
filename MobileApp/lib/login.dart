e26205d0699bb3a03be46c34d683a26bfecc1f26
Aleksandr Krotov <san.crot@yandex.ru>
2020-10-25 21:44:48 +0300

    Use Dariya login page

--- MobileApp/lib/login.dart
+++ MobileApp/lib/login.dart
@@ -6,14 +6,49 @@ import 'oauth.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login into attendance tracker'),
+    final welcomeText = Material(
+        child: Text(
+      "Welcome to Attendance App",
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 20)
+          .copyWith(color: Colors.green, fontWeight: FontWeight.bold),
+    ));
+    final loginWithIUButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.green,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () => Provider.of<OauthModel>(context).login(),
+        child: Text("Login with Innopolis University",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20)
+                .copyWith(color: Colors.white, fontWeight: FontWeight.bold)),
       ),
+    );
+
+    return Scaffold(
       body: Center(
-        child: MaterialButton(
-          child: const Text('Log In'),
-          onPressed: () => Provider.of<OauthModel>(context).login(),
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 35.0,
+                ),
+                welcomeText,
+                SizedBox(
+                  height: 25.0,
+                ),
+                loginWithIUButton
+              ],
+            ),
+          ),
         ),
       ),
     );
